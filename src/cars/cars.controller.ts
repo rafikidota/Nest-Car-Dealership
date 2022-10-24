7585df7d9992801616cff3fbbfe0a87c9902afcf
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-10-24 19:04:44 -0400

    create car service

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -23,7 +23,7 @@ export class CarsController {
 
     @Post()
     create(@Body() createCarDto: CreateCarDto) {
-        return createCarDto;
+        return this.carsService.create(createCarDto);
     }
 
     @Patch(':id')
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuid } from 'uuid';
+import { CreateCarDto } from './DTO/create-car.dto';
 
 @Injectable()
 export class CarsService {
@@ -29,6 +30,15 @@ export class CarsService {
     findOneByID(id: string) {
         const car = this.cars.find(car => car.id === id);
         if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
+        return car;
+    }
+
+    create(createCarDTo: CreateCarDto) {
+        const car: Car = {
+            id: uuid(),
+            ...createCarDTo
+        }
+        this.cars.push(car);
         return car;
     }
 }
